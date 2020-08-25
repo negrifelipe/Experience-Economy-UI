6ffeb478fb98b7211a46747f1bb44de83d1ae205
Feli <64453816+DarkTequila22@users.noreply.github.com>
2020-08-25 16:05:34 -0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using Rocket.API;
+
+namespace F.ExperienceUI
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public ushort ExperienceUI;
+        public string EconomySymbol;
+        public ushort ServerNameUI;
+        public string ServerName;
+
+        public void LoadDefaults()
+        {
+            this.ExperienceUI = (ushort)32400;
+            this.EconomySymbol = "$";
+            this.ServerNameUI = (ushort)32402;
+            this.ServerName = "Server Name Here";
+        }
+    }
+}
