c97844ce21b7b1b863d55bc1284d2d032e74a576
Feli <64453816+01-Feli@users.noreply.github.com>
2020-09-20 00:03:29 -0300

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,10 +1,11 @@
-﻿using Rocket.Core.Logging;
+using Rocket.Core.Logging;
 using Rocket.Core.Plugins;
 using Rocket.Unturned;
 using Rocket.Unturned.Events;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
 using System;
+using fr34kyn01535.Uconomy;
 
 namespace F.ExperienceUI
 {
@@ -17,24 +18,41 @@ namespace F.ExperienceUI
             Main.Instance = this;
 
 
-            Logger.Log("F.ExperienceUI Loaded", ConsoleColor.Red);
-            Logger.Log("Discord Support: https://discord.gg/6zQVJ9p", ConsoleColor.Red);
+            Logger.Log("####################################################", ConsoleColor.Red);
+            Logger.Log("#           F.Economy/ExperienceUI Loaded          #", ConsoleColor.Red);
+            Logger.Log("#                 Plugin By: Feli                  #", ConsoleColor.Red);
+            Logger.Log("#    Discord Support:https://discord.gg/6zQVJ9p    #", ConsoleColor.Red);
+            Logger.Log("#                 Plugin Ver: 3.0.0                #", ConsoleColor.Red);
+            Logger.Log("####################################################", ConsoleColor.Red);
 
 
             U.Events.OnPlayerConnected += Events_OnPlayerConnected;
             U.Events.OnPlayerDisconnected += Events_OnPlayerDisconnected;
-            UnturnedPlayerEvents.OnPlayerUpdateExperience += Events_OnPlayerUpdateExperience;
+            if (this.Configuration.Instance.UconomyMode == true)
+            {
+                Uconomy.Instance.OnBalanceUpdate += UconomyOnBalanceUpdate;
+            }
+            else { UnturnedPlayerEvents.OnPlayerUpdateExperience += Events_OnPlayerUpdateExperience; }
         }
 
         private void Events_OnPlayerConnected(UnturnedPlayer player)
         {
-            EffectManager.sendUIEffect(this.Configuration.Instance.ExperienceUI, (short)32401, player.CSteamID, true,  this.Configuration.Instance.EconomySymbol + player.Experience.ToString()); 
+            if (this.Configuration.Instance.UconomyMode == false)
+            {
+                EffectManager.sendUIEffect(this.Configuration.Instance.ExperienceUI, (short)32401, player.CSteamID, true, this.Configuration.Instance.EconomySymbol + player.Experience.ToString());
+            }
+            else { EffectManager.sendUIEffect(this.Configuration.Instance.ExperienceUI, (short)32401, player.CSteamID, true, this.Configuration.Instance.EconomySymbol + Uconomy.Instance.Database.GetBalance(player.Id).ToString()); }
             EffectManager.sendUIEffect(this.Configuration.Instance.ServerNameUI, (short)32403, player.CSteamID, true,  this.Configuration.Instance.ServerName.ToString());
         }
 
         private void Events_OnPlayerUpdateExperience(UnturnedPlayer player, uint experience)
         {
             EffectManager.sendUIEffect(this.Configuration.Instance.ExperienceUI, (short)32401, player.CSteamID, true, this.Configuration.Instance.EconomySymbol + player.Experience.ToString());
+        }
+        
+        private void UconomyOnBalanceUpdate(UnturnedPlayer player, decimal v)
+        {
+            EffectManager.sendUIEffect(this.Configuration.Instance.ExperienceUI, (short)32401, player.CSteamID, true, this.Configuration.Instance.EconomySymbol + Uconomy.Instance.Database.GetBalance(player.Id).ToString());
         }
 
         private void Events_OnPlayerDisconnected(UnturnedPlayer player)
@@ -48,6 +66,7 @@ namespace F.ExperienceUI
             U.Events.OnPlayerConnected -= Events_OnPlayerConnected;
             U.Events.OnPlayerDisconnected -= Events_OnPlayerDisconnected;
             UnturnedPlayerEvents.OnPlayerUpdateExperience -= Events_OnPlayerUpdateExperience;
+            Uconomy.Instance.OnBalanceUpdate -= UconomyOnBalanceUpdate;
         }
     }
 }
